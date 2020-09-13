2fb2c2c1ef69b312ff502795c1ba7a0cc5098d47
sneha310 <49879075+sneha310@users.noreply.github.com>
2020-09-13 23:06:09 +0530

    Update auth.service.ts

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const { BASE_URL } = process.env;
+const { BASE_URL } = 'https://pratilipi-backend.herokuapp.com';
 
 const AUTH_API = `${BASE_URL}/auth/`;
 
