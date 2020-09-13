07875b14de8d7f1ce9f1eaa1979357280835c5e1
sneha310 <49879075+sneha310@users.noreply.github.com>
2020-09-13 23:12:06 +0530

    Update user.service.ts

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const { BASE_URL } = 'https://pratilipi-backend.herokuapp.com';
+const BASE_URL = 'https://pratilipi-backend.herokuapp.com';
 
 const API_URL = `${BASE_URL}/api/`;
 
