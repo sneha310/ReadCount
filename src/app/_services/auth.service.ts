2b69c73da6f4722d98dcd8d3d1f43d223d58541a
sneha310 <49879075+sneha310@users.noreply.github.com>
2020-09-13 22:13:06 +0530

    Update auth.service.ts

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const AUTH_API = 'http://localhost:8080/api/auth/';
+const { BASE_URL } = process.env;
+
+const AUTH_API = `${BASE_URL}/auth/`;
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
